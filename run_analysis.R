830462f05ee4cdc23aea75bdc84dfaf97d6672ac
AnonimousEconomista <41473950+AnonimousEconomista@users.noreply.github.com>
2018-12-14 22:24:30 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -16,11 +16,11 @@ unzip(zipfile = "zippedFiles.zip")
 activityLabels<-read.table(file.path(Directory, "./UCI HAR Dataset/activity_labels.txt"))
 features<-read.table(file.path(Directory, "./UCI HAR Dataset/features.txt"))
 
-trainSubject<-read.table(file.path(Directory, "./UCI HAR Dataset/train/subject_train.txt"), col.names= "GroupCode")
+trainSubject<-read.table(file.path(Directory, "./UCI HAR Dataset/train/subject_train.txt"), col.names= "SubjectId")
 trainSet<-read.table(file.path(Directory, "./UCI HAR Dataset/train/X_train.txt"), col.names = features[,2])
 trainLabel<-read.table(file.path(Directory, "./UCI HAR Dataset/train/y_train.txt"), col.names = "activity")
 
-testSubject<-read.table(file.path(Directory, "./UCI HAR Dataset/test/subject_test.txt"), col.names = "GroupCode")
+testSubject<-read.table(file.path(Directory, "./UCI HAR Dataset/test/subject_test.txt"), col.names = "SubjectId")
 testSet<-read.table(file.path(Directory, "./UCI HAR Dataset/test/X_test.txt"), col.names = features[,2])
 testLabel<-read.table(file.path(Directory, "./UCI HAR Dataset/test/y_test.txt"), col.names = "activity")
 
@@ -32,7 +32,7 @@ mergedDataSet<-rbind(mergedTrain, mergedTest)
 
 #### Step 2: Extract only the measurements on the mean and standard deviation for each measurement
 
-measurements<-mergedDataSet[,c("GroupCode","activity",grep("mean|std", names(mergedDataSet), value = TRUE))]
+measurements<-mergedDataSet[,c("SubjectId","activity",grep("mean|std", names(mergedDataSet), value = TRUE))]
 
 #### Step 3: Use descriptive activity names to name the activities in the data set
 
@@ -54,7 +54,7 @@ names(measurements)<-newnames
 
 #### Step 5: From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject.
 means <- measurements %>% 
-        group_by(GroupCode, activity) %>%
+        group_by(SubjectId, activity) %>%
         summarise_all(funs(mean))
 
 tidydata<-write.table(means, file = "tidydata.txt", row.names = FALSE)
